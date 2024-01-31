83b8fcd79cd5ac1754ac3b5a31e708d02d2e8359
= <=>
2024-01-30 23:10:16 -0600

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+require("dotenv").config(); // load environment variables from .env file
+
+const express = require("express");
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+// Initiate modules
+const app = express();
+
+// Middleware
+app.use(express.json()); // parse json
+app.use(express.urlencoded({ extended: true })); // parse
+
+// Initialize Sequelize
+const sequelize = new Sequelize(
+  process.env.POSTGRES_DB, // DB name
+  process.env.POSTGRES_USER, // user name
+  process.env.POSTGRES_PASSWORD, // password
+  {
+    host: process.env.POSTGRES_HOST, // host port
+    dialect: "postgres",
+  }
+);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(8080);
